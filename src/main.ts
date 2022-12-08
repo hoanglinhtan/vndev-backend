2e598e2dc32282f297223b83ff91222fa8dfecd7
hoanglinhtan <hltan8495@gmail.com>
2022-12-08 16:27:30 +0700

    Add Monitoring

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import tracer from './monitoring/tracer';
 
 async function bootstrap() {
+  await tracer.start();
+
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
 }
---
+++ src/monitoring/tracer.ts
@@ -0,0 +1,36 @@
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { Resource } from '@opentelemetry/resources';
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const exporterOptions = {
+  url: 'http://localhost:4318/v1/traces',
+};
+
+const traceExporter = new OTLPTraceExporter(exporterOptions);
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'vndev-backend',
+  }),
+});
+
+// initialize the SDK and register with the OpenTelemetry API
+// this enables the API to record telemetry
+sdk
+  .start()
+  .then(() => console.log('Tracing initialized'))
+  .catch((error) => console.log('Error initializing tracing', error));
+
+// gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  sdk
+    .shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
+
+export default sdk;
