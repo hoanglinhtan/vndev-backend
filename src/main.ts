87efe3875cc8d7a02ecd84067c04e448be8c073d
hoanglinhtan <hltan8495@gmail.com>
2022-12-10 12:57:13 +0700

    Add Prom Metric

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppLogger } from './logger/applogger.service';
+import { collectDefaultMetrics } from 'prom-client';
 
 async function bootstrap() {
+  collectDefaultMetrics();
+
   const app = await NestFactory.create(AppModule, {
     logger: new AppLogger(),
   });
+
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
